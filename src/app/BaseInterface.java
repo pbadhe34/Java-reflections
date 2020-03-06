3d9de822e29ad53b36a15c3cdcbf48d20cc376e9
Prakash Badhe <prakash.badhe@agilesoft.in>
2020-03-06 16:34:50 +0530

    first commit

---
+++ src/app/BaseInterface.java
@@ -0,0 +1,10 @@
+package app;
+
+public interface BaseInterface {
+	
+	public int interfaceInt=0;
+	
+	void method1();
+	
+	int method2(String str);
+}
---
+++ src/app/Eating.java
@@ -0,0 +1,5 @@
+package app;
+
+public interface Eating {
+    String eats();
+}
